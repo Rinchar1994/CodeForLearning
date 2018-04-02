b3a9096b37b52c824a2b4d0bec98b87b8707ca90
Rinchar1994 <Rinchar1994@gmail.com>
2018-04-02 16:02:08 +0800

    change Concurrence File

--- src/main/java/Concurrence/Join.java
+++ src/main/java/Concurrence/Join.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.Concurrence;
 
 import java.util.concurrent.TimeUnit;
 
--- src/main/java/Concurrence/Plate.java
+++ src/main/java/Concurrence/Plate.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.Concurrence;
 
 import java.util.ArrayList;
 import java.util.List;
