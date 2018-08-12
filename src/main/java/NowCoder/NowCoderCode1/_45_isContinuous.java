9caaeca8a657883c889aab2adea09e6f27586756
rinchar <rinchar1994@gmail.com>
2018-08-12 22:42:38 +0800

    nowcoder_20180812

--- src/main/java/NowCoder/NowCoderCode1/_45_isContinuous.java
+++ src/main/java/NowCoder/NowCoderCode1/_45_isContinuous.java
@@ -1,10 +1,38 @@
 package main.java.NowCoder.NowCoderCode1;
 
+import java.util.Arrays;
+
 /**
  * Created by Rinchar on 2018/4/30.
  */
 public class _45_isContinuous {
-    public boolean isContinuous(int [] numbers) {
+    public static boolean isContinuous(int [] numbers) {
+        if (numbers.length == 0) {
+            return false;
+        }
+        Arrays.sort(numbers);
+        int num_zero = 0, gap = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == 0) {
+                num_zero++;
+                continue;
+            }
+            if(numbers[i] == numbers[i+1]) return false;
+        }
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if(numbers[i] == 0) continue;
+            gap += numbers[i + 1] == numbers[i] ? 0 : numbers[i + 1] - numbers[i] - 1;
+        }
+
+        if (gap > num_zero) {
+            return false;
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+        int[] temp = {1, 3, 0, 5, 0};
+        System.out.println(isContinuous(temp));
     }
 }
---
+++ src/main/java/NowCoder/NowCoderCode1/_46_notDown_LastRemainingSolution.java
@@ -0,0 +1,9 @@
+package main.java.NowCoder.NowCoderCode1;
+
+
+public class _46_notDown_LastRemainingSolution {
+
+    public static int LastRemaining_Solution(int n, int m) {
+        return 0;
+    }
+}
